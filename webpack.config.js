544ec488629fd76452d9bbe28dd58eae75307fd6
Luan Vinicius <nalucode@hotmail.com>
2022-03-01 00:55:43 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,24 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extesions: ['.ts', '.tsx', '.js'],
+    extesions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [
+        { loader: 'style-loader' },
+        { loader: 'sass-loader' },
+        { loader: 'css-loader', options: { modules: true } }
+      ]
+    }]
   },
   devServer: {
     contentBase: './public',
