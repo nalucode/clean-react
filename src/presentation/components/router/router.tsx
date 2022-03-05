1cff68cfbdab6843d6bbf5876682bd81ac152137
Luan Vinicius <nalucode@hotmail.com>
2022-03-05 01:10:08 -0300

    feat: add app router

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from '@/presentation/pages'
+import '@/presentation/styles/global.scss'
+
+const Test: React.FC = () => {
+  return (
+    <h1>Teste</h1>
+  )
+}
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
